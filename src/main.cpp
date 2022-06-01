6d50d494d1f127f2f0e030a152af45cfeeea5d7b
Adrian B. Hoffsten <11799746+asd20752@users.noreply.github.com>
2022-06-01 23:40:09 +0200

    Included MCP2515 library

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,26 @@
 // Orion temperature third-party: https://www.orionbms.com/manuals/pdf/third_party_thermistors.pdf
 
 #include <SPI.h>
-#include <mcp2515.h>
-  struct can_frame tempCanMessage; // To send for one peace of data. Can be duplicated for other ID's and data
-  MCP2515 mcp2515(10);             // Chip select
+#include <mcp2515.h>>
+struct can_frame tempCanMessage; // To send for one peace of data. Can be duplicated for other ID's and data
+MCP2515 can0(10);                // Chip select
+
+// Temperature Sensor for Battery Management System
+// For "Li-ion building block Li4P25RT"
+
+const int sg1 = A0;
+const int light = 13;
+int fan = 0;          // fan speed
+const int outFan = 3; // Pin out for fan PWM
+float volt[33] = {2.44, 2.42, 2.40, 2.38, 2.35, 2.32, 2.27, 2.23, 2.17, 2.11, 2.05, 1.99, 1.86, 1.80, 1.74, 1.68,
+                  1.63, 1.59, 1.55, 1.51, 1.48, 1.45, 1.43, 1.40, 1.38, 1.37, 1.35, 1.34, 1.33, 1.32, 1.31, 1.30};
+float temp[33] = {-40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120};
+
+int diff[33] = {};
+int index = 0;
+
+float sensor1 = 0;
+float temperature = 0; // temperature
 void setup()
 {
 
@@ -27,27 +44,9 @@ void setup()
   }
   SPI.begin();
 
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS); // Rate of CANBUS 500kbps for normal usage
-  mcp2515.setNormalMode();
-
-  // Temperature Sensor for Battery Management System
-  // For "Li-ion building block Li4P25RT"
-
-  const int sg1 = A0;
-  const int light = 13;
-  const int fan = 0;    // fan speed
-  const int outFan = 3; // Pin out for fan PWM
-  float volt[33] = {2.44, 2.42, 2.40, 2.38, 2.35, 2.32, 2.27, 2.23, 2.17, 2.11, 2.05, 1.99, 1.86, 1.80, 1.74, 1.68,
-                    1.63, 1.59, 1.55, 1.51, 1.48, 1.45, 1.43, 1.40, 1.38, 1.37, 1.35, 1.34, 1.33, 1.32, 1.31, 1.30};
-  float temp[33] = {-40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120};
-
-  int diff[33] = {};
-  int index = 0;
-
-  float sensor1 = 0;
-  float temperature = 0; // temperature
-
+  can0.reset();
+  can0.setBitrate(CAN_500KBPS); // Rate of CANBUS 500kbps for normal usage
+  can0.setNormalMode();
 
   Serial.begin(9600);
   Serial.println("Startup");
@@ -58,6 +57,14 @@ unsigned long tempSendTime;
 
 void loop()
 {
+  // --- Canbus receive ---
+  struct can_frame frame;
+
+  if (can0.readMessage(&frame) == MCP2515::ERROR_OK)
+  {
+    // frame contains received message
+    Serial.println(frame.can_id);
+  }
 
   sensor1 = (analogRead(sg1));
 
@@ -96,5 +103,5 @@ void loop()
 
   // --- CAN-BUS ---
   if (millis() - tempSendTime > 100)
-    mcp2515.sendMessage(&tempCanMessage);
+    can0.sendMessage(&tempCanMessage);
 }
