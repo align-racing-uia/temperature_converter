be486b2ebdc70ccaa099b55b9a4a655dd412abc1
Adrian B. Hoffsten <11799746+asd20752@users.noreply.github.com>
2022-06-28 22:50:39 +0200

    "Should" be working temp converter

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ MCP2515 can0(10);                // Chip select
 #define VOLTAGE_OFFSET -0.10
 #define LIGHT_PIN 13
 int fan = 0;          // fan speed
-#define FAN_PIN_OUT 3 // Pin out for fan PWM
 float volt[33] = {2.44, 2.42, 2.40, 2.38, 2.35, 2.32, 2.27, 2.23, 2.17, 2.11, 2.05, 1.99, 1.86, 1.80, 1.74, 1.68, 1.63, 1.59, 1.55, 1.51, 1.48, 1.45, 1.43, 1.40, 1.38, 1.37, 1.35, 1.34, 1.33, 1.32, 1.31, 1.30};
 float temp[33] = {-40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120};
 float diff[33] = {};
@@ -108,9 +107,9 @@ void loop()
   tempCanMessage.data[1] = temperatures[minIndex]; // Lowest thermistor value (8 bit signed, 1*C increments)
   tempCanMessage.data[2] = temperatures[maxIndex]; // Highest thermistor value (8 bit signed, 1*C increments)
   tempCanMessage.data[3] = avgTemp;                // Average thermistor value (8 bit signed, 1*C increments)
-  tempCanMessage.data[4] = 0x05;                   // Number of thermistors enabled
-  tempCanMessage.data[5] = maxIndex;               // Highest thermistor ID on the module
-  tempCanMessage.data[6] = minIndex;               // Lowest thermistor ID on the module
+  tempCanMessage.data[4] = 0x01;                   // Number of thermistors enabled
+  tempCanMessage.data[5] = 0x01;               // Highest thermistor ID on the module
+  tempCanMessage.data[6] = 0x00;               // Lowest thermistor ID on the module
   tempCanMessage.data[7] = (((tempCanMessage.data[0] +
                               tempCanMessage.data[1] +
                               tempCanMessage.data[2] +
@@ -118,15 +117,9 @@ void loop()
                               tempCanMessage.data[4] +
                               tempCanMessage.data[5] +
                               tempCanMessage.data[6] +
-                              8 +
-                              // FIXME Checksum might not be right Which 8 bits to use?
-                              tempCanMessage.can_id) >>
-                             8) &
+                              0x41) >> 8) & // 0x41 is a constant given from the length of bytes and 0x39 given in contact with Orion
                             0xFF); // Checksum 8-bytes sum of all bytes + ID + length https://www.orionbms.com/manuals/utility_jr/param_cell_broadcast_message.html
 
-  // --- Fan controller ---
-  int tmp = constrain(avgTemp, 25, 40);
-  fan = map(tmp, 25, 40, 0, 255);
 
   if (avgTemp >= 50)
   {
